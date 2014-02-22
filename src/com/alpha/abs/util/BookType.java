d568d4a4f73e197c818336919456ef0943b1a7a3
Dilanka_Nilan <Dilanka_Nilan@Dilanka>
2014-02-22 08:51:49 -0800

    This is Dilanaka's 3rd Commit

--- src/com/alpha/abs/util/BookType.java
+++ src/com/alpha/abs/util/BookType.java
@@ -3,11 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.alpha.abs.util;
 
 /**
  *
  * @author Dilanka_Nilan
  */
-public class NewClass {
-    
+public enum BookType {
+
+    JAVA,
+    PHP
 }
